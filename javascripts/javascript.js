4cb19cdb606cff5b11ccf1d4d12d8ffc24b4a8f1
Gonzalo Salmerón Robles <gonzxlosalmeron@gmail.com>
2021-02-25 21:08:50 +0100

    Add files via upload

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,25 +1,10 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() { scrollFunction() };
 
-// Get the button that opens the modal
-var a = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-a.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbar-logo-img").style.width = "90px";
+    } else {
+        document.getElementById("navbar-logo-img").style.width = "110px";
     }
 }
